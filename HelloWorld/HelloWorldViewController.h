6efd75c807d6aceb61b8441a634ecd5fe3334384
Trevor Reid <trevorgreid@gmail.com>
2011-04-17 17:27:19 -0400

    Initial Commit

---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Trevor Reid on 4/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController {
+    
+}
+
+@end
---
+++ HelloWorldTests/HelloWorldTests.m
@@ -0,0 +1,33 @@
+//
+//  HelloWorldTests.m
+//  HelloWorldTests
+//
+//  Created by Trevor Reid on 4/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "HelloWorldTests.h"
+
+
+@implementation HelloWorldTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HelloWorldTests");
+}
+
+@end
